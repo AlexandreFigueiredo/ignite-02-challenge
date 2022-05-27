9225f186d54f5a59e536cd4f79776066e2b73314
Alexandre Figueiredo <ale.fig1701@gmail.com>
2022-05-27 13:10:10 -0300

    first commit

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { GenreResponseProps } from "../App";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+interface ISideBarProps {
+  selectedGenreId: number;
+  buttonClick: Function;
+}
+
+export function SideBar({buttonClick, selectedGenreId}: ISideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => buttonClick(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+  )
+}
